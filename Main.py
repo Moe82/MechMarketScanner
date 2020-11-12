1ef5bb944def88c93afb9573895f00fa1eb8983a
Mohamed Shafee <mshafee1993@gmail.com>
2020-11-12 13:50:15 -0500

    fix for issue #4

--- Main.py
+++ Main.py
@@ -1,12 +1,10 @@
 from User import UserSettings
 from MarketScanner import Scanner
 
-class main:
+class App:
 
 	def __init__(self):
 		self.settings = UserSettings()
-		self.scanner = Scanner()
-		self.start()
 
 	def processKeywordString (self, string):
 		keyWordlist = string.split(",")
@@ -17,15 +15,15 @@ class main:
 				nonOptionalKeywords.append(keyword[0:len(keyword)-1])
 			else:
 				optionalKeywords.append(keyword)
-		self.scanner.optionalKeywords = optionalKeywords
-		self.scanner.nonOptionalKeywords = nonOptionalKeywords;
+		self.settings.optionalKeywords = optionalKeywords
+		self.settings.nonOptionalKeywords = nonOptionalKeywords;
 
 	def requestFlair(self):
 		print("Would you like add flair? (press enter to search without flair)")
 		flair = " "
-		while flair not in ["buying","sellin","trading", ""]:
+		while flair not in ["buying","selling","trading", ""]:
 			flair = input("[buying/selling/trading]:")
-		self.scanner.flair = flair 
+		self.settings.flair = flair 
 
 	def requestKeywords(self):
 		keywordString = input("Keywords: ")
@@ -36,7 +34,7 @@ class main:
 		print("Please enter the keywords for the product you are looking for (seperated by commas).")
 		print("If a keyword must be included, append a '+' sign to the end of it.")
 		print("\nExamples:\n\n\t Target: any Oblivion set \n\t Keywords: oblivion+ \n\n\t Terget: GMK Oblivion \n\t keywords: gmk+, oblivion+ \n\n\t Terget: Satisfaction75 \n\t keywords: satisfaction, sat75, satisfaction75, sat, 75")
-		print("After you enter the keywords, you will have the option of picking 1 of 3 flairs:")
+		print("\nAfter you enter the keywords, you will have the option of picking 1 of 3 flairs:")
 		print("\n\t-Buying\n\t-Selling\n\t-Trading")
 		print("***********************************************************************************")
 	
@@ -63,7 +61,7 @@ class main:
 				print("The default reply is 'PM!'. Would you like to set your own reply")
 				if self.inputLoop() == True:	
 					self.settings.replyMessage = input("Please enter a reply \n:")
-			print("\nWould you like to send a message to the author of posts that match your search term?")
+			print("\nWould you like to send a message to the authors of posts that match your search term?")
 			if self.inputLoop(self.settings.setSendMsgOption) == True:
 				print("The default message is 'Hello, I'm interested in (buying/trading/selling) _____'. Would you like to set your own message?")
 				if self.inputLoop() == True:	
@@ -83,13 +81,18 @@ class main:
 			print("Please enter your Reddit API credentials.")
 		while (self.settings.credentialsVerified != True):	
 			self.requestAPICredentials()
-			if self.scanner.isConnected(self.settings.getCredentialsDict()) == True:
+			if scanner.isConnected(self.settings.getCredentialsDict()) == True:
 				self.settings.credentialsVerified = True
 				self.settings.updateSettingsFile()
 		self.requestOptionsForSettings();
-		
 		self.displayInstructions();
 		self.requestKeywords();
 		self.requestFlair();
+		scanner = Scanner(self.settings)
+		scanner.start()
 
-main()
+
+
+if __name__ == "__main__":
+	app = App()
+	app.start()
